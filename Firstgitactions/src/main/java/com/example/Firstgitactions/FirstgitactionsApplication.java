63a1cab55bd178446614d122085d699e813186f0
NithinTellakula <nithintellakula2003@gmail.com>
2025-07-17 21:50:37 +0530

    "initial push"

---
+++ Firstgitactions/src/main/java/com/example/Firstgitactions/FirstgitactionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Firstgitactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstgitactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstgitactionsApplication.class, args);
+	}
+
+}
